6a21f9bd449eb52349176cc06c32db7fe134df51
Mandadapu-Kinnera <mandadapukinnera@gmail.com>
2025-12-29 11:33:20 +0530

    Use environment variables for database configuration

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+import os
+
 DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "mysql",
-    "database": "beyondchats",
-    "port": 3306
+    "host": os.getenv("DB_HOST"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME"),
+    "port": int(os.getenv("DB_PORT", 3306))
 }
