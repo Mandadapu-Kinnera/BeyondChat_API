57c4bdee6974631d1245c06a7f75c52fce997c29
Mandadapu Kinnera <167014967+Mandadapu-Kinnera@users.noreply.github.com>
2025-12-29 14:46:33 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -2,10 +2,19 @@ import os
 import mysql.connector
 
 def get_db_connection():
+    host = os.getenv("MYSQL_HOST")
+    user = os.getenv("MYSQL_USER")
+    password = os.getenv("MYSQL_PASSWORD")
+    database = os.getenv("MYSQL_DATABASE")
+    port = int(os.getenv("MYSQL_PORT", 3306))
+
+    if not all([host, user, password, database]):
+        raise Exception("Database environment variables not set")
+
     return mysql.connector.connect(
-        host=os.getenv("MYSQLHOST"),
-        user=os.getenv("MYSQLUSER"),
-        password=os.getenv("MYSQLPASSWORD"),
-        database=os.getenv("MYSQLDATABASE"),
-        port=int(os.getenv("MYSQLPORT", 3306))
+        host=host,
+        user=user,
+        password=password,
+        database=database,
+        port=port
     )
